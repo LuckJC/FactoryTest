e21638b1fb09605feae06d247c3395a7d0ca6826
huangchao <958028483@qq.com>
2020-11-12 20:15:24 +0800

    显示双卡信号。

--- app/src/main/java/com/fibocom/factorytest/NetworkFragment.java
+++ app/src/main/java/com/fibocom/factorytest/NetworkFragment.java
@@ -1,16 +1,16 @@
 package com.fibocom.factorytest;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.PhoneStateListener;
+import android.telephony.ServiceState;
 import android.telephony.SignalStrength;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,60 +19,40 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NetworkFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class NetworkFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
+    private static final String TAG = "NetworkFragment";
     private TextView mTypeTv;
     private TextView mRssiTv;
+    private TextView mRssiTv2;
     private ConnectivityManager mConnectivityManager;
     private TelephonyManager mTelephonyManager;
+    private SubscriptionManager mSubscriptionManager;
+    private List<MyPhoneStateListener> myPhoneStateListener;
+    private Map<MyPhoneStateListener, TelephonyManager> myPhoneStateListenerTelephonyManagerMap;
 
     public NetworkFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NetworkFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NetworkFragment newInstance(String param1, String param2) {
-        NetworkFragment fragment = new NetworkFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
         mConnectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         mTelephonyManager = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
-        mTelephonyManager.listen(new MyPhoneStateListener(), PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+        mSubscriptionManager = SubscriptionManager.from(getContext());
+        myPhoneStateListener = new ArrayList<>();
+        myPhoneStateListenerTelephonyManagerMap = new HashMap<>();
     }
 
     @Override
@@ -82,64 +62,127 @@ public class NetworkFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_network, container, false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         mTypeTv = view.findViewById(R.id.typeTv);
         mRssiTv = view.findViewById(R.id.rssiTv);
+        mRssiTv2 = view.findViewById(R.id.rssiTv2);
 
-        NetworkInfo activeNetInfo = mConnectivityManager.getActiveNetworkInfo();
-        if (activeNetInfo == null || !activeNetInfo.isAvailable()) {
-            return;
-        }
+        List<TextView> textList = new ArrayList<>();
+        textList.add(mRssiTv);
+        textList.add(mRssiTv2);
 
-        NetworkInfo networkInfo = mConnectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (null != networkInfo) {
-            String strSubTypeName = networkInfo.getSubtypeName();
-            mTypeTv.setText(strSubTypeName);
+        int index = 0;
+        @SuppressLint("MissingPermission") List<SubscriptionInfo> list = mSubscriptionManager.getActiveSubscriptionInfoList();
+        if (list != null && !list.isEmpty()) {
+            boolean useSeparator = false;
+            StringBuilder builder = new StringBuilder();
+            for (SubscriptionInfo subInfo : list) {
+                if (isSubscriptionInService(subInfo.getSubscriptionId())) {
+                    TelephonyManager telephonyManager = mTelephonyManager.createForSubscriptionId(subInfo.getSubscriptionId());
+                    MyPhoneStateListener listener = new MyPhoneStateListener(textList.get(index++));
+                    myPhoneStateListener.add(listener);
+                    myPhoneStateListenerTelephonyManagerMap.put(listener, telephonyManager);
+
+                    if (useSeparator) builder.append(", ");
+                    builder.append(getNetworkOperatorName(subInfo.getSubscriptionId()));
+                    useSeparator = true;
+                }
+            }
+            mTypeTv.setText(builder.toString());
+        } else {
+            //mSummary = MobileNetworkUtils.getCurrentCarrierNameForDisplay(mContext);
+            mTypeTv.setText(mTelephonyManager.getNetworkOperatorName());
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onResume() {
+        super.onResume();
+        //mTelephonyManager.listen(myPhoneStateListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+        for (MyPhoneStateListener listener : myPhoneStateListener) {
+            TelephonyManager telephonyManager = myPhoneStateListenerTelephonyManagerMap.get(listener);
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //mTelephonyManager.listen(myPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+        for (MyPhoneStateListener listener : myPhoneStateListener) {
+            TelephonyManager telephonyManager = myPhoneStateListenerTelephonyManagerMap.get(listener);
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_NONE);
+        }
+    }
+
+    public String getNetworkOperatorName(int subId) {
+        try {
+            Method method = mTelephonyManager.getClass().getMethod("getNetworkOperatorName", int.class);
+            String name = (String) method.invoke(mTelephonyManager, subId);
+            return name;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ServiceState getServiceStateForSubscriber(int subId) {
+        try {
+            Method method = mTelephonyManager.getClass().getMethod("getServiceStateForSubscriber", int.class);
+            ServiceState state = (ServiceState) method.invoke(mTelephonyManager, subId);
+            return state;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean isSubscriptionInService(int subId) {
+        if (mTelephonyManager != null) {
+            if (getServiceStateForSubscriber(subId).getState()
+                    == ServiceState.STATE_IN_SERVICE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     class MyPhoneStateListener extends PhoneStateListener {
+        TextView rssiTv;
+
+        public MyPhoneStateListener(TextView rssiTv) {
+            super();
+            this.rssiTv = rssiTv;
+        }
+
         @RequiresApi(api = Build.VERSION_CODES.M)
         @Override
         public void onSignalStrengthsChanged(SignalStrength signalStrength) {
             super.onSignalStrengthsChanged(signalStrength);
-            String signalInfo = signalStrength.toString();
-            String[] params = signalInfo.split(" ");
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            if (mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_LTE) {
-                //4G网络 最佳范围   >-90dBm 越大越好
-                //int Itedbm = Integer.parseInt(params[9]);
-                //setDBM(Itedbm + "");
-                Log.d("MyLog", "signalStrength: " + signalStrength.getLevel());
-                //mRssiTv.setText(signalStrength.getCdmaDbm());
-            } else if (mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_HSDPA ||
-                    mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_HSPA ||
-                    mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_HSUPA ||
-                    mTelephonyManager.getNetworkType() == TelephonyManager.NETWORK_TYPE_UMTS) {
-                //3G网络最佳范围  >-90dBm  越大越好  ps:中国移动3G获取不到  返回的无效dbm值是正数（85dbm）
-                //在这个范围的已经确定是3G，但不同运营商的3G有不同的获取方法，故在此需做判断 判断运营商与网络类型的工具类在最下方
-                    /*String yys = IntenetUtil.getYYS(getApplication());//获取当前运营商
-                    if (yys == "中国移动") {
-                        setDBM(0 + "");//中国移动3G不可获取，故在此返回0
-                    } else if (yys == "中国联通") {
-                        int cdmaDbm = signalStrength.getCdmaDbm();
-                        setDBM(cdmaDbm + "");
-                    } else if (yys == "中国电信") {
-                        int evdoDbm = signalStrength.getEvdoDbm();
-                        setDBM(evdoDbm + "");
-                    }*/
 
-            } else {
-                //2G网络最佳范围>-90dBm 越大越好
-                int asu = signalStrength.getGsmSignalStrength();
-                int dbm = -113 + 2 * asu;
-                //setDBM(dbm + "");
-                mRssiTv.setText("" + dbm);
+            try {
+                Method method = signalStrength.getClass().getMethod("getDbm");
+                int dbm = (int) method.invoke(signalStrength);
+                rssiTv.setText(dbm + " dbm");
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
             }
         }
     }
